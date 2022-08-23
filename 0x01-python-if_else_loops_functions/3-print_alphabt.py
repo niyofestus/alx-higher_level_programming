bf25ff8caef52b7ddfd5c17b99d62c378c28c448
NIYITANGA Festus <43537970+niyofestus@users.noreply.github.com>
2022-08-23 17:47:09 +0200

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for a in range(ord('a'), ord('z') + 1):
-    if chr(a) != 'e' and chr(a) != 'q':
-        print("{:c}".format(a), end='')
+for i in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(i), end="")
